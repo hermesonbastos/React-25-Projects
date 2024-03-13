2c83081ed9e1800cfd43e3c37bb894ae5664f603
hermesonbastos <hermesonbastos@gmail.com>
2024-03-13 14:52:01 -0300

    version 2.0

--- image_slider/src/ImageSlider.jsx
+++ image_slider/src/ImageSlider.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ArrowBigRight, ArrowBigLeft } from "lucide-react";
+import { ArrowBigRight, ArrowBigLeft, Circle, CircleDot } from "lucide-react";
 
 const ImageSlider = ({ imageUrls }) => {
   const [imageIndex, setImageIndex] = useState(0);
@@ -16,23 +16,39 @@ const ImageSlider = ({ imageUrls }) => {
     <div className="img-slider">
       <div className="img-slider-images">
         {imageUrls.map((url) => {
-          return <img key={url} src={url} className="img-slider-img" style={{ translate: `${-100 * imageIndex}%` }} />;
+          return (
+            <img
+              key={url}
+              src={url}
+              className="img-slider-img"
+              style={{ translate: `${-100 * imageIndex}%` }}
+            />
+          );
         })}
       </div>
       <button
         onClick={() => handlePrevImage()}
-        className="img-slide-btn"
+        className="img-slider-btn"
         style={{ left: 0 }}
       >
         <ArrowBigLeft />
       </button>
       <button
         onClick={() => handleNextImage()}
-        className="img-slide-btn"
+        className="img-slider-btn"
         style={{ right: 0 }}
       >
         <ArrowBigRight />
       </button>
+      <div className="img-slider-buttons">
+        {imageUrls.map((_, index) => {
+          return (
+            <button className="img-slider-dot-btn" key={index} onClick={() => setImageIndex(index)}>
+              {index === imageIndex ? <CircleDot /> : <Circle />}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 };
